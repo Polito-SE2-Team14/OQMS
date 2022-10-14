236a77c3484d52040754bd49a5d192beea9e2b88
Edoardo-Desktop <edoardocolella.it@gmail.com>
2022-10-14 17:44:53 +0200

    first part of skeleton

---
+++ server/server.js
@@ -0,0 +1,29 @@
+'use strict'
+
+//import
+const express = require('express');
+const morgan = require('morgan')
+
+
+// init express
+const app = new express();
+const port = 3001;
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+//GET /api/test
+app.get('/api/hello', (req,res)=>{
+    let message = {
+      message: 'Hello World!'
+    }
+    return res.status(200).json(message);
+  });
+  
+
+
+// activate the server
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost:${port}`);
+  });
