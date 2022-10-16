1346029a24aec8f9eff4a3002d44ef6ca0c5f553
Antonio Campobasso <campobassoantonio7@gmail.com>
2022-10-16 15:37:58 +0200

    improved layout for queue page, starting setup for receiving tickets

---
+++ client/DISCUSS.md
@@ -0,0 +1,12 @@
+# Client skeleton
+
+## Decide on interface layout
+
+## Decide on page routing
+
+# Queue Page
+
+## Decide on queue layout
+
+## Define API for receiving ticket
+
--- client/src/pages/QueuePage.js
+++ client/src/pages/QueuePage.js
@@ -1,69 +1,160 @@
-import { Container, Row, ListGroup, Badge, Col } from "react-bootstrap";
-
-import "./QueuePage.css";
+import { useState } from "react";
+import { Container, Row, ListGroup, Badge, Col, Button } from "react-bootstrap";
 
 function QueuePage() {
+	// TODO: Get data from server
+	let test_queue_list = [
+		{
+			type: "Account Management",
+			length: 13,
+		},
+		{
+			type: "Report",
+			length: 35,
+		},
+		{
+			type: "Out of ideas",
+			length: 2,
+		},
+		{
+			type: "Test",
+			length: 24,
+		},
+	];
+
+	let test_ticket_calls = [
+		{
+			number: "#A99",
+			type: "Account Management",
+			desk: "3",
+		},
+		{
+			number: "#R35",
+			type: "Report",
+			desk: "7",
+		},
+		{
+			number: "#O29",
+			type: "Out of ideas",
+			desk: "8",
+		},
+		{
+			number: "#T22",
+			type: "Test",
+			desk: "2",
+		},
+		{
+			number: "#R36",
+			type: "Report",
+			desk: "5",
+		},
+	];
+
+	let [ticket_calls, setTicketCalls] = useState(test_ticket_calls);
+	let [queue_list, setQueueList] = useState(test_queue_list);
+
 	return (
-		<Container>
-			<Row className="justify-content-center">
-				<QueueHeader />
-			</Row>
-			<Row className="justify-content-center">
-				<QueueTab />
+		<Container className="mt-3">
+			<Row>
+				<Col md="3">
+					<Row>
+						<TicketCalls ticket_calls={ticket_calls} />
+					</Row>
+				</Col>
+				<Col md={{ span: 6, offset: 2 }}>
+					<Row>
+						<QueueTab queue_list={queue_list} ticket_calls={ticket_calls} />
+					</Row>
+				</Col>
 			</Row>
 		</Container>
 	);
 }
 
-function QueueHeader() {
+function TicketCalls(props) {
 	return (
 		<Container fluid className="queue-header">
 			<Row>
 				<Col>
-					<h3>Service</h3>
+					<h4>Ticket</h4>
 				</Col>
 				<Col className="d-flex justify-content-end">
-					<h3>Queue</h3>
+					<h4>Desk</h4>
 				</Col>
+			</Row>
+			<Row>
+				<ListGroup variant="flush" className="p-0">
+					{props.ticket_calls.map((ticket, i) => {
+						return (
+							<ListGroup.Item key={i} className="d-flex justify-content-between">
+								<Container fluid>
+									<Row>
+										<Col><TicketBadge text={ticket.number}/></Col>
+										<Col className="text-end"><DeskBadge text={ticket.desk}/></Col>
+									</Row>
+								</Container>
+							</ListGroup.Item>
+						);
+					})}
+				</ListGroup>
 			</Row>
 		</Container>
 	);
 }
 
-function QueueTab() {
-	// TODO: Get number of services offered by the office
-	let service_types = ["Account management", "Report", "Out of ideas", "Test"];
+function QueueTab(props) {
+	// TODO:	Handle creation when new ticket arrives? Probably not needed
+	// 			Update corresponding queue when ticket arrives (serving ticket, desk)
+	//			Update queue whenever ticket is served (get new queue size)
 
 	return (
-		<ListGroup className="queue-container">
-			{service_types.map((service, i) => {
-				return (
-					<ListGroup.Item key={i} className="d-flex justify-content-between">
-						<Container fluid>
-							<Row>
-								<Col className="d-flex align-items-center">{service}</Col>
-								<Col>
-									Serving ticket <QueueBadge type="success" text="#99"/> <br/>
-									Desk <QueueBadge type="danger" text="7"/>
-								</Col>
-								<Col
-									md="1"
-									className="d-flex justify-content-center align-items-center"
-								>
-									<QueueBadge type="primary" text={14}/>
-								</Col>
-							</Row>
-						</Container>
-					</ListGroup.Item>
-				);
-			})}
+		<ListGroup className="p-0">
+			{props.queue_list
+				.sort((a, b) => b.length - a.length)
+				.map((queue, i) => {
+					return (
+						<ListGroup.Item key={i} className="d-flex justify-content-between">
+							<Container fluid>
+								<Row>
+									<Col className="d-flex align-items-center">{queue.type}</Col>
+									<Col>
+										Serving ticket{" "}
+										<TicketBadge text={queue.serving_ticket} />{" "}
+										<br />
+										Desk <DeskBadge text={queue.desk} />
+									</Col>
+									<Col md="2" className="text-end">
+										<QueueBadge type="primary" text={queue.length} /> <br />
+										Waiting
+									</Col>
+								</Row>
+							</Container>
+						</ListGroup.Item>
+					);
+				})}
 		</ListGroup>
 	);
 }
 
 function QueueBadge(props) {
 	return (
-		<Badge bg={props.type} pill>
+		<Badge bg="primary" pill>
+			{props.text}
+		</Badge>
+	);
+}
+
+function TicketBadge(props) {
+	return (
+		<Badge bg="success" pill>
+			{props.text}
+		</Badge>
+	);
+}
+
+function DeskBadge(props) {
+	return (
+		<Badge bg="danger" pill>
 			{props.text}
 		</Badge>
 	);
