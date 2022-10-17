bd185a4aff1c86778d0accf44c5351026c676c94
Edoardo-Desktop <edoardocolella.it@gmail.com>
2022-10-17 21:01:09 +0200

    gitignore update

--- server/database/databaseManager.js
+++ server/database/databaseManager.js
@@ -1,6 +1,6 @@
 const sqlite = require('sqlite3');
 
 // open the database
-exports.DB = new sqlite.Database('./OQMS.db', (err) => {
+exports.DB = new sqlite.Database('./server/database/OQMS.db', (err) => {
   if (err) throw err;
 });
