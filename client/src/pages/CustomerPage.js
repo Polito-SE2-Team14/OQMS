a110d800dfc10cd896a56f3316c5030dc237d70c
Edoardo Colella s257113 <s257113@studenti.polito.it>
2022-10-18 11:09:41 +0200

    some work on customer page

--- client/src/pages/CustomerPage.js
+++ client/src/pages/CustomerPage.js
@@ -1,16 +1,47 @@
-import { Container, Row, Button } from "react-bootstrap";
+import { useState } from "react";
+import { Container, Row, Button, Form } from "react-bootstrap";
 
 // Move in separate file
 function CustomerPage() {
+	
+	//call apis to get service
+	
+	const [service, setService] = useState(null);
+	const [active, setActive] = useState(false)
+	let services = [{ id: 1, name: "mailingService" }, { id: 2, name: "shipment" }];
+
+	const showTicket = () => {
+		console.log(service)
+		setActive(true)
+		setTimeout( () => setActive(false), 5000)
+	}
+
 	return <Container>
 		<Row>
 			<h1>Decide the service</h1>
 		</Row>
+		<Form>
+			{services.map(s => {
+				return <Form.Check onClick={() => setService(s)}
+					type={'radio'}
+					name={"group1"}
+					label={s.name}
+					id={s.id}
+				/>
+			})}
+		
+		</Form>
 		<Row>
-			<Button>
+			<Button onClick={() => showTicket()}>
 				Ask for a ticket
 			</Button>
 		</Row>
+
+		{(active) && <Row>
+			<h1>{service.name + " " + service.id}</h1>
+		</Row>}
+		
+
 	</Container>
 }
 
